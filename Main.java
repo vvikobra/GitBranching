f49899f9583f6d6afc57a1b356512a91d2eeebeb
vvikobra <viktoria.obrokova@yandex.ru>
2024-11-14 21:39:58 +0300

    add division

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.println(addition(1, 2));
+        System.out.println(subtract(1, 2));
+        System.out.println(multiply(1, 2));
+        System.out.println(divide(2, 1));
+
     }
 
     public static int addition(int first, int second) {
@@ -10,5 +14,13 @@ class Main {
 
     public static int subtract(int first, int second) {
         return first - second;
+    }
+
+    public static int multiply(int first, int second) {
+        return first * second;
+    }
+
+    public static int divide(int first, int second) {
+        return first / second;
     }
 }
