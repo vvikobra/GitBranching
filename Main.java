2555fe33e24f09c50feb173eefd620261737be07
vvikobra <viktoria.obrokova@yandex.ru>
2024-11-14 21:36:56 +0300

    add addition

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
-class Main{
+class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+        System.out.println(addition(1, 2));
+    }
+
+    public static int addition(int first, int second) {
+        return first + second;
     }
 }
